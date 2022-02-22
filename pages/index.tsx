f6ce729f987ec3eb2c5ce29012fd8f657e7a3812
Prakash-sa <sainiprakash525@gmail.com>
2022-02-22 15:54:24 +0530

    sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Sidebar from "../components/Sidebar"
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div >
       <Head>
         <title>Twitter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
+        <Sidebar/>
+      </main>
     </div>
   )
 }
