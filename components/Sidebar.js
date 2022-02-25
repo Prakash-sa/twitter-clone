2a933bcc407ae5aa8f23dd4e271d0b39e69c7118
Prakash-sa <sainiprakash525@gmail.com>
2022-02-25 16:25:51 +0530

    removed errors

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -21,7 +21,7 @@ function Sidebar() {
 
   return (
     <div className='hidden sm:flex flex-col items-center xl:items-start
-    xl:w-[390px] p-2 fixed h-full'>
+    xl:w-[340px] p-2 fixed h-full'>
       <div className='flex items-center justify-center w-14 h-14
       hoverAnimation p-0 xl:ml-24'>
         <Image src="https://rb.gy/ogau5a" width={30} height={30}/>
@@ -37,7 +37,9 @@ function Sidebar() {
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
       <button className='hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full
-      w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>Tweet</button>
+      w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>
+        Tweet
+      </button>
       <div className='text-[#d9d9d9] flex items-center justify-center
       hoverAnimation xl:ml-auto xl:-mr-5 mt-auto'
       onClick={signOut}
