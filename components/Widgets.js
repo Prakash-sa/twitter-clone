5049d693bc87ef84118b4ae6440dcd22cf165663
Prakash-sa <sainiprakash525@gmail.com>
2022-02-25 16:29:24 +0530

    button height correct

--- components/Widgets.js
+++ components/Widgets.js
@@ -11,7 +11,7 @@ function Widgets({trendingResults,followResults}) {
             <div className='flex items-center bg-[#202327] p-3 rounded-full relative'>
                 <SearchIcon className='text-gray-500 h-5 z-50'/>
                 <input type="text" className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border
-                border-transparent w-full focus:border-[#1d9bf0] rounded-full
+                border-transparent w-full h-full focus:border-[#1d9bf0] rounded-full
                 focus:bg-black focus:shadow-lg"
                 placeholder='Search Twitter'
                 />
